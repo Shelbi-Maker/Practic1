b15bf15a31248a675f3db717db8942998ffcad86
Shelbi-Maker <82089215+Shelbi-Maker@users.noreply.github.com>
2024-02-21 21:55:37 +0300

    Update test.py

--- test.py
+++ test.py
@@ -5,20 +5,18 @@ from app import square
 class TestSquareFunction(unittest.TestCase):
 
     def test_positive_number(self):
-        self.assertEqual(square(2), 4)  # 2 в квадрате равно 4
+        self.assertEqual(square(2), 4)  
 
     def test_negative_number(self):
-        self.assertEqual(square(-3), 9)  # (-3) в квадрате равно 9
+        self.assertEqual(square(-3), 9)  
 
     def test_zero(self):
-        self.assertEqual(square(0), 0)  # 0 в квадрате равно 0
-
+        self.assertEqual(square(0), 0)  
     def test_large_number(self):
-        self.assertEqual(square(1000), 1000000)  # 1000 в квадрате равно 1000000
+        self.assertEqual(square(1000), 1000000) 
 
     def test_fractional_number(self):
-        self.assertEqual(square(2.5), 6.25)  # 2.5 в квадрате равно 6.25
-
+        self.assertEqual(square(2.5), 6.25)  
 
 if __name__ == "__main__":
     unittest.main()
