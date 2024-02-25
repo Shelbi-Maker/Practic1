7e2020c16ff1db43ec18534158164cfdb2046387
Shelbi-Maker <82089215+Shelbi-Maker@users.noreply.github.com>
2024-02-25 18:52:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 def square(x):
     return x ** 2
 
-
 # Добавляем пустую строку после определения функции
-
-
-if __name__ == "__main__":
-    num = int(input("Введите число для возведения в квадрат: "))
-    result = square(num)
-    print(f"Квадрат числа {num} равен {result}")
