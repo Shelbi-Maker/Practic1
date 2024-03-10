9855fd65925b80c1ada8a0dba23332fa69e2e7e4
Shelbi-Maker <82089215+Shelbi-Maker@users.noreply.github.com>
2024-03-10 20:35:06 +0300

    Update and rename test.py to test_1.py

---
+++ test_1.py
@@ -0,0 +1,21 @@
+import unittest
+from app import square
+
+class TestSquare(unittest.TestCase):
+    def testsqr5(self):
+        self.assertEqual(square(5), 25)
+
+    def testsqr8(self):
+        self.assertEqual(square(8), 64)
+
+    def test_sqr_0(self):
+        self.assertEqual(square(0), 0)
+
+    def test_sqr_10(self):
+        self.assertEqual(square(10), 100)
+
+    def test_sqr_1(self):
+        self.assertEqual(square(1), 1)
+
+if __name == '__main':
+    unittest.main()
